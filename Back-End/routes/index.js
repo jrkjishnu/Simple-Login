a11f6d7c33e2a7af09a07a797c460ca19af3caa7
Jishnu <jishnurajk4@gmail.com>
2021-05-09 19:04:51 +0530

    backend completed

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
+const { route } = require('./users');
 var router = express.Router();
+
+let users = [
+  {
+    email:"abc@gmail,com",password:"password"
+  }
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Welcome to Backend Dev")
 });
+
+router.post('/login',(req,res)=>
+{
+  let result = users.find(user=>user.email === req.body.email);
+  if(result)
+  {
+    if(result.password === req.body.password)
+      {
+        res.status(200).send({message:"login successful"})
+      }
+      else
+      {
+        res.status(200).send({message:"password incorrect"})
+      }
+  }
+  else
+  {
+    res.status(200).send({message:"user doesn't exist"})
+  }
+})
 
 module.exports = router;
